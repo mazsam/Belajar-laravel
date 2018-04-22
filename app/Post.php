576ce14d7a0ef62afc34a98a5697cda18a7c02d3
mazsam <arifinsam.sam@gmail.com>
2018-04-22 22:18:17 +0700

    Elequent Model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Table Name
+    protected $table        = 'posts';
+    // Primary key
+    protected $primaryKey   = 'id';
+    // Timestamps
+    public $timestamps      = true;
 }
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@
 Route::get('/', 'PagesController@index');
 Route::get('about','PagesController@about');
 Route::get('service','PagesController@service');
+
+Route::resource ('posts','PostsController');
